36910fbbb705470861ddb7d8b257e4be0900fadf
ahu <1540303002@qq.com>
2020-04-23 15:55:16 +0800

    自定义url&数据库创建

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,9 +1,29 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,jsonify
 
 rooter_user = Blueprint('user_page',__name__)
-@rooter_user.route("/login")
+@rooter_user.route("/login",methods=['get','post'])
 def login():
-    return render_template('user/login.html')
+    if request.method == 'GET':
+        return render_template('user/login.html')
+    resp = {
+        'code':200,
+        'msg':'登录成功！',
+        'data':{}
+    }
+    req = request.values
+    login_name = req['login_name']
+    login_pwd = req['login_pwd']
+    # 后端校检 不为空 长度不小于1
+    if login_name is None or len(login_name) < 1:
+        resp['code'] = -1
+        resp['msg'] = '请输入正确的用户名！'
+        return jsonify(resp)
+    if login_pwd is None or len(login_pwd) < 1:
+        resp['code'] = -1
+        resp['msg'] = '请输入正确的密码'
+        return jsonify(resp)
+
+    return jsonify(resp)
 
 @rooter_user.route('/logout')
 def logout():
--- web/static/js/user/login.js
+++ web/static/js/user/login.js
@@ -2,16 +2,45 @@
 var user_login_ops = {
     init:function(){
         this.eventBind()
-        console.log('aaaa')
     },
     eventBind:function(){
         $('.login_wrap .do-login').click(function(){
-            console.log('click')
-            var login_name = $('.login_wrap input[name=login_name]').val()
+            // btn
+            var btn_target = $(this)
+            // 判断disabled样式是否存在
+            if(btn_target.hasClass("disabled")){
+                alert('请不要重复提交！')
+            }
+            var login_name = $('.login_wrap input[name=login_name]').val()  // 获取用户名和密码的值
             var login_pwd = $('.login_wrap input[name=login_pwd]').val()
 
-            console.log(login_name)
-            console.log(login_pwd)
+            // 前端校检，不为空，长度不下于1
+            if(login_name == undefined || login_name.length < 1){
+                alert('您输入的用户名为空')
+                return;   
+            }
+            if(login_pwd == undefined || login_pwd.length < 1){
+                alert('您输入的密码为空')
+                return;
+            }
+            btn_target.addClass("disabled")  // 满足前面校检之后  点击登录会添加一个disabled的一个样式
+
+            // Ajax 用来前后端分离的 ,数据格式是json {key:value}
+            // 前端发送的数据是json格式，所以后端response 返回的也得是json格式
+            $.ajax({
+                url:"/user/login",
+                type:"POST",
+                data:{"login_name":login_name,"login_pwd":login_pwd},
+                dataType:"json",
+                success:function(reponse){
+                    btn_target.removeClass('disabled')
+                    console.log(reponse)
+                }
+            })
+            
+
+
+
         })
     }
 }
